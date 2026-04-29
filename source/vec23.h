8e52dc162df0b871e50fc7dcf027e2aaa5443f93
Anxietytimmy <71737812+Anxietytimmy@users.noreply.github.com>
2026-04-29 15:58:01 -0700

    I got too silly

--- source/vec23.h
+++ source/vec23.h
@@ -318,3 +318,164 @@ inline vec3x4 clamp4(const vec3x4& v, float minv, float maxv){
         vminnmq_f32(vmaxnmq_f32(v.z, lo), hi)
     };
 }
+
+// NEW ARRIVALS IN INDIA 
+// Maybe its those horse people I was talking about 
+// Or thier cousins or something
+
+// snap back to reality
+// Or well I guess randomness
+
+// ACTUALLY FUCKING VECTORIZED RNG THIS TIME GOOD GOD
+// -------------------------------
+// V E C R N G N E O N 
+// -------------------------------
+
+// 4-Lane XOR Shift (mainly so trace doesn't blow its head off)
+inline uint32x4_t xorshift4(uint32x4_t& s) {
+    s = veorq_u32(s, vshlq_n_u32(s, 13));
+    s = veorq_u32(s, vshrq_n_u32(s, 17));
+    s = veorq_u32(s, vshlq_n_u32(s,  5));
+    return s;
+}
+
+// Convert uint32x4 into a float 1, to match scalar rng in trace
+inline float32x4_t toFloat01(uint32x4_t x) {
+    uint32x4_t m = vandq_u32(x, vdupq_n_u32(0xFFFFFF));
+    return vmulq_n_f32(vcvtq_f32_u32(m), 1.0f / float(0xFFFFFF));
+}
+
+// Vec cosines/sines, were stuck on a different planet
+// -----------------------------------------------
+// F A S T S I N / C O S ( N E O N )
+// -----------------------------------------------
+
+// This technically has a .3% error rate, but I cannot be fucked to care
+
+// We use horner's method of evaluating polynomials.
+// Where a polynomial with degree N can be evaluated only using n mul/adds
+// We could simply plug in n value to find at every polynomial variable, however this is much faster
+// Take p(x) = 3x³ + 2x² - 5x + 1, we could simply plug in our desired X value, however instead
+// We can nest the polynomial to reduce the number of mul needed, ie
+// p(x) = 3x³ + 2x² - 5x + 1 --> (3x + 2)x² - 5x + 1 --> ((3x + 2)x + (-5))x + 1
+// To get p(x) = (((3x + 2)x - 5)x + 1), reducing the required multiplications
+
+// Taylor seires are considered, but we reduce their range and flip sign for full (0, 2pi)
+
+
+// Sin4
+inline float32x4_t sin4(float32x4_t phi) {
+    // Pi constants
+    const float32x4_t pi = vdupq_n_f32(3.14159265f);
+    const float32x4_t pi_2 = vdupq_n_f32(1.57079633f);
+    const float32x4_t two_pi = vdupq_n_f32(6.28318531f);
+
+    // Reduce [0, 2pi] -> [0, pi], track signs
+    uint32x4_t neg = = vcgeq_f32(phi, pi);
+    float32x4_t xr = vbslq_f32(neg, vsubq_f32(phi, pi), phi);
+
+    // Reduce [0, pi] -> [0, pi/2]
+    // Sin is symmetric @ pi/2
+    uint32x4_t fold = vcgtq_f32(xr, pi_2);
+    xr = vbslq_f32(fold, vsubq_f32(pi, xr), xr);
+    
+    // Remember the horner yap from earlier, yeah here it is, n = 7
+    // in other words, fuck the moon
+    // in other words, degree 7 horner on [0, pi/2]
+    // le actual formulent
+    // sin(x) = x*(1 - x^2 * (1/6 - x^2 * (1/120 - x^2 / 5040)))
+
+    float32x4_t x2 = vmulq_f32(xr, xr);
+    float32x4_t r = vfmsq_f32(vdupq_n_f32(1.0f/120.0f), x2, vdupq_n_f32(1.0f/5040.0f));
+    r = vfmsq_f32(vdupq_n_f32(1.0f/6.0f), x2, r);
+    r = vfmsq_f32(vdupq_n_f32(1.0f), x2, r);
+    r = vmulq_f32(xr, r);
+
+    // Flip sign for lanes that were [pi, 2pi]
+    uint32x4_t signBit = vshlq_n_u32(neg, 31);
+    return vreinterpretq_f32_u32(veorq_u32(vreinterpretq_u32_f32(r), signBit));    
+}
+
+//Cos4
+inline float32x4_t cos4(float32x4_t phi) {
+    // cos(phi) = sin(phi + pi/2), wrap it into [0, 2pi]
+    float32x4_t shifted = vaddq_f32(phi, vdupq_n_f32(1.57079633f));
+    float32x4_t two_pi = vdupq_n_f32(6.28318531f);
+    uint32x4_t wrap = vcgeq_f32(shifted, two_pi);
+    shifted = vbslq_f32(wrap, vsubq_f32(shifted, two_pi), shifted);
+    return sin4(shifted);
+}
+
+// yall ready for linear algebra 
+// Orthonormal bases, also known as, what the shit was Euler on man
+// So, ONB are a set of vectors that are both orthogonal to each other and normalized such that L = 1
+// Meaning that for any two vectors (u and v), the dot product is given by u * v = 0 if u does not equal v and u * u = 1
+// Orthogonal in this case refers to two vectors which are perpendicular to each other
+
+// -----------------------------------------------
+// O N B
+// -----------------------------------------------
+
+// Uses branchless targets + bitangents from normals
+// handles n.z = -1 with/o singulatiy
+inline void buildONB4(const vec3x4& n, vec3x4& t, vec3x4& b){
+    const float32x4_t one = vdupq_n_f32(1.0f);
+    const float32x4_t neg = vdupq_n_f32(-1.0f);   
+
+    // sign = (n.z >= 0) ? 1 : -1
+    uint32x4_t pos = vcgeq_f32(n.z, vdupq_n_f32(0.0f));
+    float32x4_t sgn = vbslq_f32(pos, one, neg);
+
+    // a = -1 / (sign + n.z) - set denominators as never zeros for unit normals
+    float32x4_t denom = vaddq_f32(sgn, n.z);
+    float32x4_t r = vrecpeq_f32(denom);
+    r = vmulq_f32(vrecpsq_f32(denom, r), r);
+    r = vmulq_f32(vrecpsq_f32(denom, r), r);
+    float32x4_t a = vnegq_f32(r);
+
+    // bcoef = nx * ny * a
+    float32x4_t bc = vmulq_f32(vmulq_f32(n.x, n.y), a);
+
+    // tan = (1 + sign*nx²*a,  sign*bcoef,  -sign*nx)
+    t.x = vaddq_f32(one, vmulq_f32(sgn, vmulq_f32(vmulq_f32(n.x, n.y), a)));
+    t.y = vmulq_f32(sgn, bc);
+    t.z = vnegq_f32(vmulq_f32(sgn, nx));
+
+    // bitan = (bcoef,  sign + ny²*a,  -ny)
+    b.x = bc;
+    b.y = vaddq_f32(sgn, vmulq_f32(vmulq_f32(n.y, n.y), a));
+    b.z = vnegq_f32(n.y);
+}
+
+// hell
+// -----------------------------------------------
+// C O S H E M I S P H E R E S A M P L E
+// -----------------------------------------------
+
+// Takes normal and rng states, returns cos weighted bounce directions
+// no normalizations needed as we provide in unit lenght
+inline vec3x4 cosineSampleHemisphere4(const vec3x4& n, uint32x4_t& rng){
+    // 2 uniform floats per plane
+    float32x4_t u1 = toFloat01(xorshift4(rng));
+    float32x4_t u2 = toFloat01(xorshift4(rng));
+
+    // r = sqrt(u1), phi = 2*pi*u2
+    float32x4_t r = vrsqrtq_f32(u1);
+    float32x4_t phi = vmulq_n_f32(u2, 6.28318531f);
+
+    // local disk coord + hemisphere projection
+    float32x4_t lx = vmulq_f32(r, cos4(phi));
+    float32x4_t ly = vmulq_f32(r, sin4(phi));
+    float32x4_t lz = vsqrtq_f32(vmaxq_f32(vsubq_f32(vdupq_n_f32(1.0f), u1), vdupq_n_f32(0.0)));
+
+    // build ONB from surface mat
+    vec3x4 t, b;
+    buildOBN4(n, t, b);
+
+    // use world space 
+    vec3x4 d;
+    d.x = vaddq_f32(vaddq_f32(vmulq_f32(lx, t.x), vmulq_f32(ly, b.x)), vmulq_f32(lz, n.x));
+    d.y = vaddq_f32(vaddq_f32(vmulq_f32(lx, t.y), vmulq_f32(ly, b.y)), vmulq_f32(lz, n.y));
+    d.z = vaddq_f32(vaddq_f32(vmulq_f32(lx, t.z), vmulq_f32(ly, b.z)), vmulq_f32(lz, n.z));
+    return d;
+}
